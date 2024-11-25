1f629625c79c91546a9c8f03a4fcb119d05726c3
Filippo Marcon <filippomarcon66@gmail.com>
2024-11-25 08:54:54 +0100

    check no connection between Client/Server and C/S

--- src/config_loader.rs
+++ src/config_loader.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::{collections::{HashMap, HashSet}, fmt};
 
 use crossbeam_channel::{unbounded, Sender};
 use wg_2024::{
@@ -8,6 +8,22 @@ use wg_2024::{
     network::NodeId,
     packet::Packet,
 };
+
+enum NodeKind{
+    Drone,
+    Server,
+    Client
+}
+
+impl fmt::Display for NodeKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            NodeKind::Drone => write!(f, "Drone"),
+            NodeKind::Server => write!(f, "Server"),
+            NodeKind::Client => write!(f, "Client"),
+        }
+    }
+}
 
 use crate::dummy_nodes::ClientServerOptions;
 
@@ -26,30 +42,31 @@ pub fn config_to_options(
     let mut simcontr = SimControllerOptions {
         command_send: HashMap::new(),
         command_recv: simcontroller_command_receiver,
+        packet_send: HashMap::new(),
         config: config.clone(),
     };
 
     let mut edges: HashSet<(NodeId, NodeId)> = HashSet::new();
 
-    let mut tmp_sender_for_node: HashMap<NodeId, Sender<Packet>> = HashMap::new();
-    let mut node_ids_set : HashSet<NodeId> = HashSet::new();
+    let mut tmp_node_kind : HashMap<NodeId,NodeKind> = HashMap::new();
+    let mut tmp_node_ids_set : HashSet<NodeId> = HashSet::new();
 
-    let mut check_id_uniqueness = |id:NodeId| if node_ids_set.contains(&id) {panic!("error in toml config: two nodes with id {}",id)} else { node_ids_set.insert(id)};
-    // could be added to simulation controller
-
+    let mut check_id_uniqueness = |id:NodeId| if tmp_node_ids_set.contains(&id) {panic!("error in toml config: two nodes with id {}",id)} else { tmp_node_ids_set.insert(id)};
+    
     // the 3 following for cycles have a lot of shared code,
     // but I think it's better to wait to see if there are protocol changes before trying to optimize them
 
     // todo: check that there are no duplicate ids
     for n in config.drone.iter() {
         check_id_uniqueness(n.id);
-        
+        tmp_node_kind.insert(n.id, NodeKind::Drone);
+
         let (sim_send_command, node_receive_command) = unbounded::<Command>();
         let (node_send_packet, node_receive_packet) = unbounded::<Packet>();
 
         simcontr.command_send.insert(n.id, sim_send_command);
         // saves the channel on which the drone receives packets to use it later when creating edges
-        tmp_sender_for_node.insert(n.id, node_send_packet);
+        simcontr.packet_send.insert(n.id, node_send_packet);
 
         drones.insert(
             n.id,
@@ -70,13 +87,14 @@ pub fn config_to_options(
 
     for n in config.client.iter() {
         check_id_uniqueness(n.id);
+        tmp_node_kind.insert(n.id, NodeKind::Client);
 
         let (sim_send_command, node_receive_command) = unbounded::<Command>();
         let (node_send_packet, node_receive_packet) = unbounded::<Packet>();
 
         simcontr.command_send.insert(n.id, sim_send_command);
         // saves the channel on which the drone receives packets to use it later when creating edges
-        tmp_sender_for_node.insert(n.id, node_send_packet);
+        simcontr.packet_send.insert(n.id, node_send_packet);
 
         clients.insert(
             n.id,
@@ -96,13 +114,14 @@ pub fn config_to_options(
 
     for n in config.server.iter() {
         check_id_uniqueness(n.id);
+        tmp_node_kind.insert(n.id, NodeKind::Server);
 
         let (sim_send_command, node_receive_command) = unbounded::<Command>();
         let (node_send_packet, node_receive_packet) = unbounded::<Packet>();
 
         simcontr.command_send.insert(n.id, sim_send_command);
         // saves the channel on which the drone receives packets to use it later when creating edges
-        tmp_sender_for_node.insert(n.id, node_send_packet);
+        simcontr.packet_send.insert(n.id, node_send_packet);
 
         servers.insert(
             n.id,
@@ -120,6 +139,8 @@ pub fn config_to_options(
         }
     }
 
+    let get_kind_from_id = |id:NodeId| if drones.contains_key(&id) {NodeKind::Drone} else if clients.contains_key(&id) {NodeKind::Client} else {NodeKind::Server};
+
     while let Some((from, to)) = edges.iter().copied().next() {
         // check that edge respects bidirectionality
         if !edges.contains(&(to, from)) {
@@ -127,19 +148,17 @@ pub fn config_to_options(
             panic!("initialization file is incorrect, as it does not represent a bidirectional graph: edge from {} to {} but no corresponding edge in node {}",from,to,to);
         }
 
+        if let (Some(from_kind), Some(to_kind)) = (tmp_node_kind.get(&from),tmp_node_kind.get(&to)){
+            if !matches!(from_kind, NodeKind::Drone) && !matches!(to_kind, NodeKind::Drone) {
+                panic!("Initialization file is incorrect, as there is an edge between {} and {}, which are of type {} and {}, which isn't allowed", from, to, from_kind, to_kind);
+            } 
+        }  
+        // todo: do we need to consider the else case? 
+        // considering that in this function it should(?) never happen
+        
         // creates the two connections between nodes of the edge
-        add_sender_to_node(&mut drones, &mut clients, &mut servers, &from,to, tmp_sender_for_node.get(&to).unwrap().clone());
-        add_sender_to_node(&mut drones, &mut clients, &mut servers,&to, from, tmp_sender_for_node.get(&from).unwrap().clone());
-        // drones
-        //     .get_mut(&from)
-        //     .unwrap()
-        //     .packet_send
-        //     .insert(to, tmp_sender_for_node.get(&to).unwrap().clone());
-        // drones
-        //     .get_mut(&to)
-        //     .unwrap()
-        //     .packet_send
-        //     .insert(from, tmp_sender_for_node.get(&from).unwrap().clone());
+        add_sender_to_node(&mut drones, &mut clients, &mut servers, &from,to, simcontr.packet_send.get(&to).unwrap().clone());
+        add_sender_to_node(&mut drones, &mut clients, &mut servers,&to, from, simcontr.packet_send.get(&from).unwrap().clone());
 
         // remove both representations of edge
         edges.remove(&(from, to));
